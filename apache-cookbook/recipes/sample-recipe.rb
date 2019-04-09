91806f122ce7cef1eedefc230613ebd2c5d550c1
root <root@ip-172-31-14-14.ap-south-1.compute.internal>
2019-04-09 05:06:09 +0000

    test

---
+++ apache-cookbook/recipes/sample-recipe.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook:: apache-cookbook
+# Recipe:: sample-recipe
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
+
+#['httpd', 'mariadb-server', 'unzip', 'git', 'vim'].each do |p|
+#  package p do
+#    action :install
+#  end
+#end
+
+%w(httpd mariadb-server unzip git vim).each do |p|
+  package p do
+    action :install
+  end
+end
+
